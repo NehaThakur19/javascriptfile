8bdf6b3ee6e9d3cffa1083d2ff5003ff39ce6c0a
ManojThakur19 <mnjthakur19@gmail.com>
2024-02-14 19:40:09 +0545

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+//arithmatic operator
+
+let a=5;
+let b= 6;
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log("a**b=",a**b);
+
+
+
+// unary operators 
+console.log("a++=",a);
+console.log("++a=",++a);
+console.log("a--=",a--);
+console.log("--a=",--a);
+
+//assignment operators
+
+
+
+
+//logical operator
